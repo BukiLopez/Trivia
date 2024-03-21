5b7ee2e11f2005abf4b178c3e6cf0a7a6e0bf2f5
Ever789 <evericardo.pd@gmail.com>
2024-03-21 07:38:33 -0600

    Nuevo footer

---
+++ trivia/src/Comps/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css';
+function Footer() {
+  return (
+    <footer>
+      <p>© 2024</p>
+      <p>Authors: Marco López, Hassam Juárez, Ever Ponce y Saul Rodríguez</p>
+      <p><a href="/">marcoxx1@hotmail.com</a></p>
+      <p><a href="/Game">Contáctanos</a></p>
+    </footer>
+  );
+}
+
+export default Footer;
