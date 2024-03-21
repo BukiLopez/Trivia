b7fbdb96aa3b19892d555beee134084f5eeec3df
Marco Lopez <a1123250023@utch.edu.mx>
2024-03-21 07:27:11 -0600

    Se añadieron Game y Main como componentes, creados en la carpeta Comps y se instaló react router dom

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
-
+import Footer from './Comps/Footer.jsx';
+import Header from './Comps/Header.jsx';
+import Main from './Comps/Main.jsx';
+import Game from './Comps/Game.jsx';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/Game" element={<Game />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
