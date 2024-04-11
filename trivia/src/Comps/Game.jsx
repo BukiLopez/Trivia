1d749e4d0cc42d1725605b7f3dbe02fce1b3277e
Marco Lopez <a1123250023@utch.edu.mx>
2024-04-11 08:42:12 -0600

    Prueba local del json, falta agregar la api pero ya funciona

--- trivia/src/Comps/Game.jsx
+++ trivia/src/Comps/Game.jsx
@@ -1,16 +1,91 @@
 import './Game.css';
+import { useState } from 'react';
 function Game() {
+    const preguntas = [
+        {
+          pregunta: "¿De qué juego es este mapa?",
+          imagen: "/Mapas/Crosscode.png",
+          respuestas: [
+            { texto: "Crosscode", correcta: true },
+            { texto: "Dead Cells", correcta: false },
+            { texto: "Hades", correcta: false },
+            { texto: "Undertale", correcta: false }
+          ]
+        },
+        {
+            pregunta: "¿De qué juego es este mapa?",
+            imagen: "/Mapas/GoW.jpg",
+            respuestas: [
+              { texto: "God of War", correcta: false },
+              { texto: "Red Dead Redemption II", correcta: true },
+              { texto: "Spiderman", correcta: false },
+              { texto: "TLOZ Breath of the Wild", correcta: false }
+            ]
+          },
+          {
+            pregunta: "¿De qué juego es este mapa?",
+            imagen: "/Mapas/HaloInfinite.jpg",
+            respuestas: [
+              { texto: "Halo Infinite", correcta: true },
+              { texto: "Halo 4", correcta: false },
+              { texto: "Battlefield", correcta: false },
+              { texto: "CoD", correcta: false }
+            ]
+          },
+          {
+            pregunta: "¿De qué juego es este mapa?",
+            imagen: "/Mapas/Hollowknight.jpg",
+            respuestas: [
+              { texto: "Ever", correcta: true },
+              { texto: "Marco", correcta: false },
+              { texto: "Saul", correcta: false },
+              { texto: "Hassam", correcta: false }
+            ]
+          },
+          {
+            pregunta: "¿De qué juego es este mapa?",
+            imagen: "/Mapas/Mhw.jpg",
+            respuestas: [
+              { texto: "Monster Hunter World", correcta: true },
+              { texto: "Monster Hunter Rise", correcta: false },
+              { texto: "Slime Rancher", correcta: false },
+              { texto: "Pokémon Esmeralda", correcta: false }
+            ]
+          },
+          {
+            pregunta: "¿De qué juego es este mapa?",
+            imagen: "/Mapas/Slimerancher.jpg",
+            respuestas: [
+              { texto: "Slime Rancher 2", correcta: false },
+              { texto: "Slime Rancher", correcta: true },
+              { texto: "Dragon's Dogma 2", correcta: false },
+              { texto: "Dragon Quest", correcta: false }
+            ]
+          },
+        // más preguntas aquí
+      ];
+    const [indicePreguntaActual, setIndicePreguntaActual] = useState(0);
+    const preguntaActual = preguntas[indicePreguntaActual];
+
+    function aleatorio() {
+        return Math.floor(Math.random() * preguntas.length);
+    }
+    const handleNextQuestion = () => {
+        setIndicePreguntaActual(aleatorio());
+    };
   return (
-        <div className="game-container">
-            <h1 className="title">The Game</h1>
-            <div className="imagen">Aquí va una imagen</div>
-            <div className="answers-container">
-                <button className="selection-button">Respuesta A</button>
-                <button className="selection-button">Respuesta B</button>
-                <button className="selection-button">Respuesta C</button>
-                <button className="selection-button">Respuesta D</button>
-            </div>
+    <div className="game-container">
+        <h1 className="title">{preguntaActual.pregunta}</h1>
+        <div className="imagen"><img src={preguntaActual.imagen} alt="mapa del juego" /></div>
+        <div className="answers-container">
+            {preguntaActual.respuestas.map((respuesta, index) => (
+                <button key={index} className="selection-button">
+                    {respuesta.texto}
+                </button>
+            ))}
         </div>
+        <button onClick={handleNextQuestion}>Siguiente pregunta</button>
+    </div>
   );
 }
 
---
+++ trivia/src/Comps/GameBlur.jsx
@@ -0,0 +1,17 @@
+import './GameBlur.css';
+function GameBlur() {
+  return (
+        <div className="game-container">
+            <h1 className="title">The Game</h1>
+            <div className="imagen">Aquí va una imagen</div>
+            <div className="answers-container">
+                <button className="selection-button">Respuesta A</button>
+                <button className="selection-button">Respuesta B</button>
+                <button className="selection-button">Respuesta C</button>
+                <button className="selection-button">Respuesta D</button>
+            </div>
+        </div>
+  );
+}
+
+export default GameBlur;
