88ccbdb7bf397baf7863f290f324d4f9a849b53a
Linx306 <Hassam306@gmail.com>
2024-04-12 07:26:58 -0700

    Agrego puntuacion
    Se agregaro puntuacion y que al presionar los botones pase a la siguiente

--- trivia/src/Comps/Game.jsx
+++ trivia/src/Comps/Game.jsx
@@ -1,39 +1,51 @@
 import './Game.css';
 import { useState, useEffect } from 'react';
+
 function Game() {
     const [preguntas, setPreguntas] = useState([]);
-    useEffect(() => {
-          fetch("https://getpantry.cloud/apiv1/pantry/eab0f25a-9d10-4fdb-9af3-b49c71d9f3c9/basket/trivia")
-              .then((response) => response.json())
-              .then((resasjson) => setPreguntas(resasjson.preguntas))
-              .catch((err) => console.log(err))
-      }, []);
     const [indicePreguntaActual, setIndicePreguntaActual] = useState(0);
+    const [puntos, setPuntos] = useState(0);
+
+    useEffect(() => {
+        fetch("https://getpantry.cloud/apiv1/pantry/eab0f25a-9d10-4fdb-9af3-b49c71d9f3c9/basket/trivia")
+            .then((response) => response.json())
+            .then((resasjson) => setPreguntas(resasjson.preguntas))
+            .catch((err) => console.log(err))
+    }, []);
+
     const preguntaActual = preguntas[indicePreguntaActual];
 
     function aleatorio() {
         return Math.floor(Math.random() * preguntas.length);
     }
-    const handleNextQuestion = () => {
+
+    const handleAnswerClick = (respuesta) => {
+        if (respuesta.correcta) {
+            // Sumar un punto si la respuesta es correcta
+            setPuntos(puntos + 1);
+        }
+        // Pasar a la siguiente pregunta
         setIndicePreguntaActual(aleatorio());
     };
+
     if (!preguntaActual) {
-      return null;
+        return null;
     }
-  return (
-    <div className="game-container">
-        <h1 className="title">{preguntaActual.pregunta}</h1>
-        <div className="imagen"><img src={preguntaActual.imagen} alt="mapa del juego" /></div>
-        <div className="answers-container">
-            {preguntaActual.respuestas.map((respuesta, index) => (
-                <button key={index} className="selection-button">
-                    {respuesta.texto}
-                </button>
-            ))}
+
+    return (
+        <div className="game-container">
+            <h1 className="title">{preguntaActual.pregunta}</h1>
+            <div className="imagen"><img src={preguntaActual.imagen} alt="mapa del juego" /></div>
+            <div className="answers-container">
+                {preguntaActual.respuestas.map((respuesta, index) => (
+                    <button key={index} className="selection-button" onClick={() => handleAnswerClick(respuesta)}>
+                        {respuesta.texto}
+                    </button>
+                ))}
+            </div>
+            <h2>Puntos: {puntos}</h2>
         </div>
-        <button onClick={handleNextQuestion}>Siguiente pregunta</button>
-    </div>
-  );
+    );
 }
 
 export default Game;
