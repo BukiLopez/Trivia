808e5d980e9f118262b17c0570d9938c1060f12d
Linx306 <Hassam306@gmail.com>
2024-04-11 22:41:33 -0700

    Rutas
    Se agregaron las rutas de los botones para que al presionar el boton lleve al videojuego

--- trivia/src/Comps/Main.jsx
+++ trivia/src/Comps/Main.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './Main.css';
 function Main() {
   return (
@@ -5,10 +7,19 @@ function Main() {
       <h1 className='title'>¿ESTÁS LISTO?</h1>
       <p>Selecciona un modo de juego</p>
       <div className='buttons-container'>
+      <Link to="Game">
         <button className='selection-button'>Portadas</button>
+        </Link>
+        <Link to="Game">
         <button className='selection-button'>Mapas</button>
+        </Link>
+        <Link to="Game">
         <button className='selection-button'>Armas</button>
-        <button className='selection-button'>Invetarios</button>
+        </Link>
+        <Link to="Game">
+        <button className='selection-button' >Invetarios</button>
+        </Link>
+        
       </div>
     </div>
   );
