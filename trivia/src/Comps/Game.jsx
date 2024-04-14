e3210e7a2d9bfaced9b000490831df68200b47ef
Ever789 <evericardo.pd@gmail.com>
2024-04-14 08:56:01 -0700

    Revisa respuesta
    Tres preguntas, revisa si la respuesta es correcta

--- trivia/src/Comps/Game.jsx
+++ trivia/src/Comps/Game.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './Game.css';
+import logo from './Yipi.jpeg';
+import { Link } from 'react-router-dom';
 
 function Game() {
     const [preguntas, setPreguntas] = useState([]);
     const [indicePreguntaActual, setIndicePreguntaActual] = useState(0);
     const [puntos, setPuntos] = useState(0);
+    const [preguntasRespondidas, setPreguntasRespondidas] = useState(0);
     const [respuestaClickeada, setRespuestaClickeada] = useState(null);
+    const [mostrarJuegoTerminado, setMostrarJuegoTerminado] = useState(false);
 
     useEffect(() => {
         fetch("https://getpantry.cloud/apiv1/pantry/eab0f25a-9d10-4fdb-9af3-b49c71d9f3c9/basket/trivia")
@@ -22,17 +26,34 @@ function Game() {
 
     const handleAnswerClick = (respuesta, index) => {
         if (respuesta.correcta) {
-            // Sumar un punto si la respuesta es correcta
             setPuntos(puntos + 1);
         }
-        // Almacenar el índice de la respuesta clickeada
+        setPreguntasRespondidas(preguntasRespondidas + 1);
         setRespuestaClickeada(index);
-        // Esperar un segundo antes de pasar a la siguiente pregunta
-        setTimeout(() => {
-            setRespuestaClickeada(null);
-            setIndicePreguntaActual(aleatorio());
-        }, 1000);
+        if (preguntasRespondidas < 2) {
+            setTimeout(() => {
+                setRespuestaClickeada(null);
+                setIndicePreguntaActual(aleatorio());
+            }, 1000);
+        } else {
+            setTimeout(() => {
+                setMostrarJuegoTerminado(true);
+            }, 1000);
+        }
     };
+
+    if (mostrarJuegoTerminado) {
+        return (
+            <div className="game-container">
+                <h1>¡Juego terminado!</h1>
+                <h2>Tu puntuación total es: {puntos}</h2>
+                <p></p>
+                <img src={logo} alt='Imagen de victoria'></img>
+                <p></p>
+                <Link to="/"><button className='selection-button'>Volver al menu</button></Link> 
+            </div>
+        );
+    }
 
     if (!preguntaActual) {
         return null;
